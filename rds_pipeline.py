926cd6841cf0ce4ea76a64531ddadf65aa11a23e
br0steen <osteensco@gmail.com>
2022-04-24 17:43:18 -0400

    structure for geodata and loading into db

--- rds_pipeline.py
+++ rds_pipeline.py
@@ -3,8 +3,8 @@ import logging
 import pandas as pd
 import requests
 import time
-
-
+import bs4 as bs
+from sqlalchemy import create_engine, types
 
 
 class DataSource:
@@ -12,6 +12,8 @@ class DataSource:
         self.source = None
         self.format = None
         self.df = pd.DataFrame()
+        self.table_name = None
+        self.dtype = {}
         self.ingested_data = None
         self.destination = None
         self.scheduled = self.schedule()
@@ -31,11 +33,24 @@ class DataSource:
     def clean(self):
         pass
 
-    def load(self, secret):
+    def load(self, secret):#move inside pipeline?
         #attain secrets from aws secret manager
+        secret_hostname = ''
+        secret_db = ''
+        secret_uname = ''
+        secret_pass = ''
         #connect to aws rds database
+        hostname=secret_hostname
+        dbname=secret_db
+        uname=secret_uname
+        pwd=secret_pass
+
+        # Create SQLAlchemy engine for connection to MySQL Database
+        engine = create_engine(f'''mysql://{uname}:{pwd}@{hostname}/{dbname}''')
+
         #land in appropriate tables
-        pass
+        self.df.to_sql(self.table_name, engine, if_exists='append', index=False, dtype=self.dtype)
+
 
     def pull(self):
         pass
@@ -119,12 +134,71 @@ class WeatherData(DataSource):
     def pull(self):
         self.extract()
 
+
+
+
 class GeoData(DataSource):
     def __init__(self) -> None:
         super().__init__()
         self.source = '''https://www.unitedstateszipcodes.org/'''
         self.format = 'webscraped html to json'
+        self.df = pd.DataFrame(columns=['ZIP Code', 'County', 'State'], dtype=str)
+        self.States = [
+            'AL', 'AR', 'AZ', 'CA', 'CO', 'CT',
+            'DE', 'FL', 'GA', 'IA', 'ID', 'IL',
+            'IN', 'KS', 'KY', 'LA', 'MA', 'MD',
+            'ME', 'MI', 'MN', 'MO', 'MS', 'MT',
+            'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+            'NV', 'NY', 'OH', 'OK', 'OR', 'PA',
+            'RI', 'SC', 'SD', 'TN', 'TX', 'UT',
+            'VA', 'VT', 'WA', 'WI', 'WV', 'WY'
+        ]
         #read state abbreviations from database?
+
+    def extract(self):
+        for state in self.States:
+            r = requests.get(url=f'''https://www.unitedstateszipcodes.org/{state.lower()}/#zips-list''', 
+            headers={'''User-Agent''': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'''})
+            try:
+                r.raise_for_status()
+            except Exception as ex:
+                logging.error(getattr(ex, 'message', repr(x)))
+                continue
+            soup = bs.BeautifulSoup(r.text, 'html.parser')
+            zips = []
+            counties = []#these lists will contain the data for each column, since we loop it will maintain correct order for rows
+            sts = []
+            ZipSoup = soup.find_all('div', class_="col-xs-12 prefix-col1")#list of dirty zip data
+            CountySoup = soup.find_all('div', class_="col-xs-12 prefix-col4")#list of dirty county data
+            DirtySoup = [ZipSoup, CountySoup]
+            for each in DirtySoup:
+                for x in each:
+                    if each.index(x) != 0: #each[0] is the header, we want to bypass this
+                        x = x.get_text()#beautiful soup method, returns html data as text
+                        x = x.translate({ord(char): None for char in ['\r', '\n', '\t']})#clean html formatting text
+                        if each is ZipSoup:#put data in correct list
+                            zips.append(x)
+                        elif each is CountySoup:#put data in correct list
+                            x = x.removesuffix(" County")
+                            counties.append(x)
+                    else:#bypass header
+                        continue
+            if len(counties) != len(zips):#these should always equal, otherwise will end program and print below info
+                logging.error(f'''Shape Error found for state {state}''')
+                logging.info(f'''{len(counties)} rows for counties''')
+                logging.info(f'''{len(zips)} rows for zips''')
+            else:
+                for i in range(len(counties)):#we're just adding the current State we're on for each row that exists
+                    sts.append(state)
+                table = {'ZIP Code': zips, 'County': counties, 'State': sts}#attach our data to column names
+                df1 = pd.DataFrame(data=table, dtype=str)#put in dataframe to easily append
+                self.df = self.df.append(df1)#add to our dataframe
+                logging.info(f'''{state} Zips and Counties scraped successfully!''')
+
+
+
+
+
 
 class Pipeline:
     def __init__(self, sources) -> None:
