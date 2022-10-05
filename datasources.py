ad28ab0ca037351c757588d8ed2233a4549f9328
br0steen <osteensco@gmail.com>
2022-10-04 21:22:22 -0400

    added first college football datasource

--- datasources.py
+++ datasources.py
@@ -45,6 +45,10 @@ class DataSource:
         self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
         logging.info(f'''{type(self).__name__} loaded into {self.table_name}''' )
 
+    def truncate(self):
+        query = f'''TRUNCATE TABLE {self.dataset}{self.table_name}'''
+        result = self.db_engine.query(query).result()
+        logging.info(f'''{type(self).__name__} - {result}''')
 
 
 
@@ -218,7 +222,7 @@ class GeoData(DataSource):
     def __init__(self) -> None:
         super().__init__()
         self.source = '''https://www.unitedstateszipcodes.org'''
-        self.format = 'webscraped html to json'
+        self.format = 'webscraped html'
         self.table_name = 'US_Zips_Counties'
         self.dtypes = [
             bigquery.SchemaField('ZIP_Code', 'STRING'),
@@ -299,10 +303,118 @@ class GeoData(DataSource):
                 logging.info(f'''{state} Zips and Counties scraped successfully!''')
 
     def load(self):
-        loadjob = bigquery.LoadJobConfig(schema=self.dtypes)
-        loadjob.write_disposition = 'WRITE_TRUNCATE'
+        loadjob = bigquery.LoadJobConfig(schema=self.dtypes, write_disposition='WRITE_TRUNCATE')
+        self.truncate()
         self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
         logging.info(f'''Replaced table, {self.table_name} now up to date''')
+
+
+
+
+
+class CFTeamsAll(DataSource):
+    def __init__(self) -> None:
+        super().__init__()
+        self.source = 'https://www.espn.com/college-football/'
+        self.format = 'webscraped html'
+        self.table_name = 'Power_5_Schools'
+        self.dtypes = [
+            bigquery.SchemaField('Conference', 'STRING'),
+            bigquery.SchemaField('Team', 'STRING'),
+            bigquery.SchemaField('URL', 'STRING')
+        ]
+
+        self.dataset = '''portfolio-project-353016.collegeFootball.'''
+
+    def schedule(self):
+        super().schedule()
+        
+        query = f"""SELECT table_id FROM `{self.dataset}__TABLES__`"""
+        tbls = self.db_engine.query(query).result().to_dataframe()
+        tbls = tbls['table_id'].tolist()
+        if self.table_name in tbls:
+            query = f'''SELECT DATE(TIMESTAMP_MILLIS(last_modified_time)) AS dt FROM `{self.dataset}__TABLES__` where table_id = '{self.table_name}' '''
+            result = self.db_engine.query(query).result().to_dataframe()
+            result = result['dt'].tolist()[0]
+            if datetime.datetime(result.year+1, result.month, result.day) <= datetime.datetime.today():#schedule to run annually
+                logging.info(f'''{type(self).__name__} scheduled''')
+
+                return True
+            else:
+                logging.info(f'''{type(self).__name__} not scheduled''')
+                return False
+        else:
+            logging.info(f'''{self.table_name} not found in dataset tables, scheduling pull''')
+            return True #schedule pull if table doesn't exist in database
+
+
+    def extract(self):
+        r = requests.get(url=f'{self.source}teams', 
+            headers={'''User-Agent''': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'''})
+        try:
+            r.raise_for_status()
+        except Exception as ex:
+            logging.error(getattr(ex, 'message', repr(ex)))
+        soup = bs.BeautifulSoup(r.text, 'html.parser')
+        p5 = [
+            'ACC', 
+            'Big 12', 
+            'Pac-12', 
+            'Big Ten', 
+            'SEC'
+            ]#conferences we want data from
+        #generate list of conference tables
+        parentsoup = soup.find_all('div', class_="mt7")
+        allteams = []
+        for s in parentsoup:
+            conf = s.find('div', class_="headline headline pb4 n8 fw-heavy clr-gray-01").get_text()
+            if conf in p5:
+                confteams = []
+                #loop through teams in conf
+                teams = s.find_all('div', class_="pl3")
+                for t in teams:
+                #find_all teams.get_text and urls
+                    tm = {
+                        'Conference': conf, 
+                        'Team': t.find('h2', class_="di clr-gray-01 h5").get_text(), 
+                        'URL': t.find('a', class_="AnchorLink").get('href')
+                        }
+                    confteams.append(tm)
+                #add completed confrence to p5 dictionary
+                allteams += confteams                
+        self.df = self.df.from_records(allteams)
+
+    def load(self):
+        loadjob = bigquery.LoadJobConfig(schema=self.dtypes, write_disposition='WRITE_TRUNCATE')
+        self.truncate()
+        self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
+        logging.info(f'''Replaced table, {self.table_name} now up to date''')
+
+
+    def test(self):
+        pass
+        
+        
+
+
+
+
+
+
+class CFGamesAll(DataSource):#Table containing meta game data Columns: year, opponent, url(for game id)
+    def __init__(self) -> None:
+        super().__init__()
+
+
+
+
+
+
+
+class CFGameTeamStats(DataSource):#Table containing data from game "team stats" page data, one row for each team, Columns: url(for game id), team, Home/Away col for each stat
+    def __init__(self) -> None:
+        super().__init__()
+
 
 
 
@@ -348,5 +460,7 @@ class WebsiteEndpoint(DataSource):
 
 
 if __name__ == '__main__':
+    CFTeamsAll().test()
+    
     pass
     #add any tests here
