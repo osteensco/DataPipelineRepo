bccb4c63aad5947497ea7d18e7e0322717892d7c
br0steen <osteensco@gmail.com>
2022-10-08 23:50:06 -0400

    finished CFGames object, gamestats WIP

--- datasources.py
+++ datasources.py
@@ -8,7 +8,7 @@ from google.cloud import bigquery
 from google.api_core import exceptions as gbq
 import logging
 import sys
-
+import re
 
 
 class DataSource:
@@ -22,6 +22,7 @@ class DataSource:
         self.scheduled = None #Boolean flag used by pipeline to determine if data should be pulled or not
         self.overwrite = None #date passed to Delete query for manually scheduled data pulls, avoids duplicate entries
         self.APIkey = False
+        self.testitem = None
         self.dataset = '''portfolio-project-353016.ALL.'''
 
     def schedule(self):
@@ -50,8 +51,24 @@ class DataSource:
         result = self.db_engine.query(query).result()
         logging.info(f'''{type(self).__name__} - {result}''')
 
+    def getreq(self, url):
+        r = requests.get(url=url, 
+        headers={'''User-Agent''': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'''})
+        try:
+            r.raise_for_status()
+        except Exception as ex:
+            logging.error(getattr(ex, 'message', repr(ex)))
+        return r
 
-
+    def test(self):
+        self.schedule()
+        self.extract()
+        if self.df.shape[0] > 0:
+            print(self.df.head()) 
+        elif self.testitem:
+            print(self.testitem)
+        else:
+            print('testitem/df failed to generate')
 
         
 class WeatherData(DataSource):
@@ -263,13 +280,7 @@ class GeoData(DataSource):
 
     def extract(self):
         for state in self.States:
-            r = requests.get(url=f'''{self.source}/{state.lower()}/#zips-list''', 
-            headers={'''User-Agent''': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'''})
-            try:
-                r.raise_for_status()
-            except Exception as ex:
-                logging.error(getattr(ex, 'message', repr(ex)))
-                continue
+            r = self.getreq(f'''{self.source}/{state.lower()}/#zips-list''')
             soup = bs.BeautifulSoup(r.text, 'html.parser')
             zips = []
             counties = []#these lists will contain the data for each column, since we loop it will maintain correct order for rows
@@ -321,7 +332,8 @@ class CFTeamsAll(DataSource):
         self.dtypes = [
             bigquery.SchemaField('Conference', 'STRING'),
             bigquery.SchemaField('Team', 'STRING'),
-            bigquery.SchemaField('URL', 'STRING')
+            bigquery.SchemaField('URL', 'STRING'),
+            bigquery.SchemaField('ID', 'STRING')
         ]
 
         self.dataset = '''portfolio-project-353016.collegeFootball.'''
@@ -338,7 +350,6 @@ class CFTeamsAll(DataSource):
             result = result['dt'].tolist()[0]
             if datetime.datetime(result.year+1, result.month, result.day) <= datetime.datetime.today():#schedule to run annually
                 logging.info(f'''{type(self).__name__} scheduled''')
-
                 return True
             else:
                 logging.info(f'''{type(self).__name__} not scheduled''')
@@ -349,12 +360,7 @@ class CFTeamsAll(DataSource):
 
 
     def extract(self):
-        r = requests.get(url=f'{self.source}teams', 
-            headers={'''User-Agent''': '''Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36'''})
-        try:
-            r.raise_for_status()
-        except Exception as ex:
-            logging.error(getattr(ex, 'message', repr(ex)))
+        r = self.getreq(f'{self.source}teams')
         soup = bs.BeautifulSoup(r.text, 'html.parser')
         p5 = [
             'ACC', 
@@ -377,7 +383,8 @@ class CFTeamsAll(DataSource):
                     tm = {
                         'Conference': conf, 
                         'Team': t.find('h2', class_="di clr-gray-01 h5").get_text(), 
-                        'URL': t.find('a', class_="AnchorLink").get('href')
+                        'URL': t.find('a', class_="AnchorLink").get('href'),
+                        'ID': [m.group(1) for m in [re.search('_/id/(.+?)/', t.find('a', class_="AnchorLink").get('href'))] if m][0]
                         }
                     confteams.append(tm)
                 #add completed confrence to p5 dictionary
@@ -391,30 +398,252 @@ class CFTeamsAll(DataSource):
         logging.info(f'''Replaced table, {self.table_name} now up to date''')
 
 
-    def test(self):
-        pass
-        
-        
 
 
 
 
 
 
-class CFGamesAll(DataSource):#Table containing meta game data Columns: year, opponent, url(for game id)
-    def __init__(self) -> None:
+
+class CFGames(DataSource):#Table containing meta game data Columns: year, opponent, url(for game id)
+    def __init__(self, massgrab=False, overwrite=False) -> None:
         super().__init__()
+        self.source = 'https://www.espn.com'
+        self.massgrab = massgrab
+        self.overwrite = overwrite
+        self.format = 'webscraped html'
+        self.table_name = 'Power_5_Games'
+        self.dtypes = [
+            bigquery.SchemaField('YEAR', 'STRING'),
+            bigquery.SchemaField('GameDate', 'STRING'),
+            bigquery.SchemaField('GameID', 'STRING'),
+            bigquery.SchemaField('HomeTeam', 'STRING'),
+            bigquery.SchemaField('AwayTeam', 'STRING'),
+            bigquery.SchemaField('GameURL', 'STRING'),
+        ]
+        self.dataset = '''portfolio-project-353016.collegeFootball.'''
 
+    def schedule(self):
+        super().schedule()
+        if not self.massgrab and not self.overwrite and not self.scheduled:
+            query = f"""SELECT table_id FROM `{self.dataset}__TABLES__`"""
+            tbls = self.db_engine.query(query).result().to_dataframe()
+            tbls = tbls['table_id'].tolist()
+            if self.table_name in tbls:
+                tdy = datetime.datetime.today()
+                if tdy.month in [8,9,10,11,12,1] and tdy.weekday() == 0:#if its college football season and a monday then auto pull latest game data
+                    logging.info(f'''{type(self).__name__} scheduled''')
+                    return True
+                else:
+                    logging.info(f'''{type(self).__name__} not scheduled''')
+                    return False
+            else:
+                self.massgrab = True
+                logging.info(f'''{self.table_name} not found in dataset tables, scheduling pull''')
+                return True #schedule pull if table doesn't exist in database           
+        else:
+            logging.info(f'''{type(self).__name__} scheduled manually''')
+            return True
 
+    def extract(self):
+        allgames = []
+        query = f"""SELECT * FROM {self.dataset}{self.table_name}"""
+        p5schls = self.db_engine.query(query).result().to_dataframe()
+        power5teams = [{'id': i['ID'], 'path': i['URL']} for i in p5schls.to_dict('records')]
+
+        #pulling max year from the team page is easy, just using good ol TN since it doesn't matter which team is used
+        path = f'''{self.source}/college-football/team/schedule/_/id/2633'''
+        r = self.getreq(path)
+        maxyr = int(max([y.get_text() for y in bs.BeautifulSoup(r.text, 'html.parser').find_all('option', class_="dropdown__option")]))
+        years = []
+        #2010-2022 if mass grab otherwise just current year
+        if not self.massgrab:
+            yr = maxyr
+        else:
+            yr = 2010
+        while yr <= maxyr:
+            years.append(str(yr))
+            yr += 1
+
+        for team in power5teams:
+            for year in years:
+                path = f'''{self.source}/college-football/team/schedule/_/id/{team['id']}/season/{year}'''
+                r = self.getreq(path)
+                soup = bs.BeautifulSoup(r.text, 'html.parser').find_all('tr')
+                for s in soup:
+                    game = {
+                            "YEAR": year, 
+                            "GameDate": '',
+                            "GameID": '', 
+                            "HomeTeam": '', 
+                            "AwayTeam": '',
+                            "GameURL": ''
+                            }
+                    #pulls in game id url
+                    gmid = s.find('span', class_="ml4")
+                    if gmid:
+                        game["GameDate"] = s.td.span.get_text()
+                        gmurl = gmid.find('a', class_="AnchorLink").get('href')
+                        game["GameURL"] = gmurl
+                        game["GameID"] = gmurl.split("gameId/")[1]
+                        if s.find('span', class_="pr2").get_text() == 'vs':
+                            game["HomeTeam"] = team['path']
+                            game["AwayTeam"] = s.find('a', class_="AnchorLink").get('href')
+                        else:
+                            game["HomeTeam"] = s.find('a', class_="AnchorLink").get('href')
+                            game["AwayTeam"] = team['path']
+                        allgames.append(game)
+        self.df = self.df.from_records(allgames)
+        self.df = self.df.drop_duplicates()
+        if not self.overwrite:
+            self.df = self.clean_and_append(self.df, years)
+
+    def clean_and_append(self, df, years):
+        query = f"""SELECT * FROM {self.dataset}{self.table_name} WHERE YEAR IN {tuple(years) if len(years) > 1 else f'''('{years[0]}')'''}"""
+        qdf = self.db_engine.query(query).result().to_dataframe()
+        df = pd.concat([df, qdf], ignore_index=True)
+        df = df.drop_duplicates(keep=False)
+        return df
+
+    def load(self):
+        loadjob = bigquery.LoadJobConfig(schema=self.dtypes)
+        if self.overwrite:
+            self.truncate()
+            loadjob.write_disposition = 'WRITE_TRUNCATE'
+        else:
+            loadjob.write_disposition = 'WRITE_APPEND'
+        loadjob.schema_update_options = [bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION]
+        self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
+        logging.info(f'''{type(self).__name__} loaded into {self.table_name}''' )
 
 
 
 
 
 class CFGameTeamStats(DataSource):#Table containing data from game "team stats" page data, one row for each team, Columns: url(for game id), team, Home/Away col for each stat
-    def __init__(self) -> None:
+    def __init__(self, massgrab=False, overwrite=False) -> None:
         super().__init__()
+        self.source = 'https://www.espn.com/matchup?gameId='
+        self.massgrab = massgrab
+        self.overwrite = overwrite
+        self.format = 'webscraped html'
+        self.table_name = 'Game_Team_Stats'
+        self.dtypes = [
+            bigquery.SchemaField('GameID', 'STRING'),
+            bigquery.SchemaField('Team', 'STRING'),
+            bigquery.SchemaField('isHome', 'STRING'),
+            bigquery.SchemaField('Points', 'STRING'),
+            bigquery.SchemaField('FirstDowns', 'STRING'),
+            bigquery.SchemaField('ThirdDownEff', 'STRING'),
+            bigquery.SchemaField('FourthDownEff', 'STRING'),
+            bigquery.SchemaField('TotalYds', 'STRING'),
+            bigquery.SchemaField('PassYds', 'STRING'),
+            bigquery.SchemaField('PassCompAtt', 'STRING'),
+            bigquery.SchemaField('PassIntThrown', 'STRING'),
+            bigquery.SchemaField('RushYds', 'STRING'),
+            bigquery.SchemaField('RushAtt', 'STRING'),
+            bigquery.SchemaField('Penalties', 'STRING'),
+            bigquery.SchemaField('FumblesLost', 'STRING'),
+            bigquery.SchemaField('PossTime', 'STRING'),
+        ]
+        self.dataset = '''portfolio-project-353016.collegeFootball.'''
 
+    def schedule(self):
+        super().schedule()
+        if not self.overwrite and not self.scheduled:
+            query = f"""SELECT table_id FROM `{self.dataset}__TABLES__`"""
+            tbls = self.db_engine.query(query).result().to_dataframe()
+            tbls = tbls['table_id'].tolist()
+            if self.table_name in tbls:
+                tdy = datetime.datetime.today()
+                if tdy.month in [8,9,10,11,12,1] and tdy.weekday() == 0:#if its college football season and a monday then auto pull latest game data
+                    logging.info(f'''{type(self).__name__} scheduled''')
+                    return True
+                else:
+                    logging.info(f'''{type(self).__name__} not scheduled''')
+                    return False
+            else:
+                self.massgrab = True
+                logging.info(f'''{self.table_name} not found in dataset tables, scheduling pull''')
+                return True #schedule pull if table doesn't exist in database           
+        else:
+            logging.info(f'''{type(self).__name__} scheduled manually''')
+            return True
+
+    def extract(self):
+
+        allgamestats = []
+        query = f"""
+            With CurrStatsCTE AS (
+                SELECT
+                    GameID,
+                    COUNT(DISTINCT Team) AS cntcheck
+                FROM `{self.dataset}{self.table_name}`
+                GROUP BY 1
+                )
+            SELECT
+                GameID
+            FROM (
+                SELECT
+                    GameID,
+                    cntcheck
+                FROM `{self.dataset}Power_5_Games`
+                LEFT JOIN CurrStatsCTE
+                USING(GameID)
+                )
+            WHERE cntcheck < 2 
+            OR cntcheck IS NULL
+        """
+        gms = self.db_engine.query(query).result().to_dataframe()
+        gameids = [i for i in gms['GameID']]
+
+        for id in gameids:
+            path = f'''{self.source}{id}'''
+            r = self.getreq(path)
+            soup = bs.BeautifulSoup(r.text, 'html.parser')
+            for s in soup.find_all('tr'):
+                game = {
+                        'GameID': '',
+                        'Team': '',
+                        'isHome': '',
+                        'Points': '', 
+                        'FirstDowns': '', 
+                        'ThirdDownEff': '', 
+                        'FourthDownEff': '', 
+                        'TotalYds': '',
+                        'PassYds': '', 
+                        'PassCompAtt': '', 
+                        'PassIntThrown': '', 
+                        'RushYds': '', 
+                        'RushAtt': '',
+                        'Penalties': '',
+                        'FumblesLost': '', 
+                        'PossTime': ''
+                        }
+                
+                #loop through <tr> tags to grab data
+                #middle aka left col is always away team, rightmost home team
+
+                
+            #after loop grab team names and scores from <div> class_="team away/hom --> <div> class_="team__content"
+
+
+            allgamestats.append(game)
+        self.df = self.df.from_records(allgamestats)
+
+
+
+
+    def load(self):
+        loadjob = bigquery.LoadJobConfig(schema=self.dtypes)
+        if self.overwrite:
+            self.truncate()
+            loadjob.write_disposition = 'WRITE_TRUNCATE'
+        else:
+            loadjob.write_disposition = 'WRITE_APPEND'
+        loadjob.schema_update_options = [bigquery.SchemaUpdateOption.ALLOW_FIELD_ADDITION]
+        self.db_engine.load_table_from_dataframe(self.df, f'''{self.dataset}{self.table_name}''', loadjob).result()
+        logging.info(f'''{type(self).__name__} loaded into {self.table_name}''' )
 
 
 
@@ -448,7 +677,6 @@ class WebsiteEndpoint(DataSource):
         logging.info(f'''{type(self).__name__} Payload: \n{self.payload}''' )
         self.df = pd.read_json(self.payload)
         
-
     def load(self):
         #land in appropriate tables
         loadjob = bigquery.LoadJobConfig(schema=self.dtypes)
@@ -460,7 +688,10 @@ class WebsiteEndpoint(DataSource):
 
 
 if __name__ == '__main__':
-    CFTeamsAll().test()
+    run = CFGames()
+    run.test()
+
+
     
     pass
     #add any tests here
